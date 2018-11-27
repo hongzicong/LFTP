b95724f7794afb0db814031237744d76ef6ea812
hongzicong <973138379@qq.com>
2018-11-27 17:53:28 +0800

    TCP construction

--- client/LFTP.py
+++ client/LFTP.py
@@ -1,19 +1,54 @@
 # -*- coding: utf-8 -*-
 import socket
 import sys
+import random
+
+def sendFile(file):
+    pass
+
+def receiveFile():
+    pass
+
+def startTimer():
+    pass
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Argument number should be 3 instead of %d" % (len(sys.argv) - 1))
+        sys.exit(1)
     funcName = sys.argv[1]
     serverName = sys.argv[2]
     fileName = sys.argv[3]
+    defaultPort = 5555
 
     fileSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     if funcName == "lsend":
-        fileSocket.sendto("Hello world!", (serverName, 5555))
-        receiveData = fileSocket.recv(1024).decode('utf-8')
-        pass
+        with open(fileName, "r") as file:
+            # TCP construction
+            # First hand shake
+            # For safety, seq is picked randomly
+            SYN = 1
+            ACK = 1
+            SEQ = random.randint(-100, 100)
+
+            # * is the character used to split
+            # datagram format: SYN * seq
+            fileSocket.sendto(b"%d*%d*%d" % (SYN, ACK, SEQ), (serverName, defaultPort))
+
+            # Third hand shake
+            data, addr = fileSocket.recvfrom(1024)
+            SYN, ACK, SEQ = list(map(int, data.split(b"*")))
+            SYN = 0
+            ACK, SEQ = SEQ, ACK
+            ACK += 1
+            fileSocket.sendto(b"%d*%d*%d*%s" % (SYN, ACK, SEQ))
+
+            sendFile(file)
     elif funcName == "lget":
-        pass
+        with open(fileName, "w") as file:
+            fileSocket.sendTo("hello", (serverName, defaultPort))
+            receiveData, addr = fileSocket.recvfrom(1024).decode('utf-8')
+            receiveFile(file)
     else:
         print("Your input parameter is wrong!")
--- server/LFTP.py
+++ server/LFTP.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 import socket
-
+import random
 
 if __name__ == "__main__":
     fileSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     fileSocket.bind(('127.0.0.1', 5555))
+
+    print("============ Start LFTP server ============")
+
+    addr_info = {}
+
     while True:
         data, addr = fileSocket.recvfrom(1024)
-        fileSocket.sendto(b'Hello!', addr)
+
+        # TCP construction : SYN is 1
+        if data[0] == b'1':
+            # Second hand shake
+            SYN, ACK, SEQ = list(map(int, data.split(b"*")))
+            ACK = SEQ + 1
+            SEQ = random.randint(-100, 100)
+
+            #  New a buffer for the address
+            addr_info[addr] = b""
+
+            fileSocket.sendto(b"%d*%d*%d" % (SYN, ACK, SEQ), addr)
+
+            receiveData, addr = fileSocket.recvfrom(1024)
+
+        # SYN is 0
+        if data[0] == b'0':
+            pass
+
     fileSocket.close()
